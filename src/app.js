565f96a79e17990f621e34dcef7059c5ac4db79f
deepaman sharma <amandeepsharma2510@gmail.com>
2026-07-21 20:40:35 +0530

    project start

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.post("/resume",(req,res)=>{
+
+
+    red.status(201).json({
+        message :"all done"
+    })
+
+})
+
+
+
+module.exports = app;
